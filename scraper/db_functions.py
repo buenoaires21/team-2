7a420eebe5b5b2cba641e7e6bccd91f3f04f129a
giulianofichera <giulifii@hotmail.com>
2021-04-30 14:36:25 -0300

    Possibility to choose job name

--- scraper/db_functions.py
+++ scraper/db_functions.py
@@ -6,8 +6,11 @@ def preformat_data(data):
 def save_to_db(preformatted_data):
     pass
 
+def convert_to_json(unformatted_data):
+    
+
 def save_to_text_file(data_list):
     
-    with open("scraped.txt", 'w') as output:
+    with open("scraped.txt", 'a') as output:
         for row in data_list:
             output.write(str(row) + '\n')
